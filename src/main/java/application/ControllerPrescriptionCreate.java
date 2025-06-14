88e2b17233d49071cded52e8c4c1edaa1626a75a
Ethan <ethangarcia0927@gmail.com>
2025-06-13 21:31:57 -0700

    Created placeholders

---
+++ src/main/java/application/ControllerPrescriptionCreate.java
@@ -0,0 +1,12 @@
+package application;
+
+public class ControllerPrescriptionCreate {
+  /*TODO: Copy this class from Lab 19 and modify
+     Use ControllerDoctor.java for reference
+     Add Entity classes and Repository
+     Add Instance variables for SequenceService and repository interfaces
+
+     You may make changes to any of the model classes and repository interfaces as needed and add any customized query methods
+   */
+
+}
