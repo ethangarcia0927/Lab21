3fb30d67908326626b407aee01ca9f1bacc019db
midemundia <matthias.idemundia@gmail.com>
2025-06-17 21:07:32 -0700

    Update ControllerPrescriptionFill

--- src/main/java/application/ControllerPrescriptionFill.java
+++ src/main/java/application/ControllerPrescriptionFill.java
@@ -1,6 +1,15 @@
 package application;
 
-public class ControllerPrescriptionFill {
+import application.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import view.PrescriptionView;
+
+import java.util.Optional;
+
   /*TODO: Copy this class from Lab 19 and modify
      Use ControllerDoctor.java for reference
      Add Entity classes and Repository
@@ -9,4 +18,62 @@ public class ControllerPrescriptionFill {
      You may make changes to any of the model classes and repository interfaces as needed and add any customized query methods
    */
 
+@Controller
+public class ControllerPrescriptionFill {
+
+  @Autowired
+  private PrescriptionRepository prescriptionRepository;
+
+  @Autowired
+  private DrugRepository drugRepository;
+
+  @Autowired
+  private PharmacyRepository pharmacyRepository;
+
+  @GetMapping("/prescription/fill")
+  public String getFillForm(Model model) {
+    model.addAttribute("prescription", new PrescriptionView());
+    return "prescription_fill";
+  }
+
+  @PostMapping("/prescription/fill")
+  public String processFillForm(PrescriptionView view, Model model) {
+    Optional<Prescription> presc = prescriptionRepository.findById(view.getRxid());
+    if (presc.isEmpty()) {
+      model.addAttribute("message", "Prescription not found.");
+      model.addAttribute("prescription", view);
+      return "prescription_fill";
+    }
+
+    Pharmacy pharmacy = pharmacyRepository.findByNameAndAddress(view.getPharmacyName(), view.getPharmacyAddress());
+    if (pharmacy == null) {
+      model.addAttribute("message", "Pharmacy not found.");
+      model.addAttribute("prescription", view);
+      return "prescription_fill";
+    }
+
+    Optional<Drug> drugOpt = drugRepository.findById(presc.get().getRxid());
+    if (drugOpt.isEmpty()) {
+      model.addAttribute("message", "Drug not found.");
+      model.addAttribute("prescription", view);
+      return "prescription_fill";
+    }
+    Drug drug = drugOpt.get();
+
+    double cost = 0.0;
+    for (Pharmacy.DrugCost dc : pharmacy.getDrugCosts()) {
+      if (dc.getDrugName().equalsIgnoreCase(drug.getName())) {
+        cost = dc.getCost();
+        break;
+      }
+    }
+
+    view.setCost(String.format("%.2f", cost));
+    view.setDrugName(drug.getName());
+    view.setPharmacyPhone(pharmacy.getPhone());
+
+    model.addAttribute("message", "Prescription filled.");
+    model.addAttribute("prescription", view);
+    return "prescription_show";
+  }
 }
