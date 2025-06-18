00a521fd887c031eae9717e06ddac5c4e0a71e1a
Ethan <ethangarcia0927@gmail.com>
2025-06-17 20:44:13 -0700

    Removed TODOs

--- src/main/java/application/ControllerPatientCreate.java
+++ src/main/java/application/ControllerPatientCreate.java
@@ -16,15 +16,6 @@ import java.sql.*;
 
 @Controller
 public class ControllerPatientCreate {
-  /*TODO: Copy this class from Lab 19 and modify
-     Use ControllerDoctor.java for reference
-     Add Entity classes and Repository
-     Add Instance variables for SequenceService and repository interfaces
-
-     You may make changes to any of the model classes and repository interfaces as needed and add any customized query methods
-   */
-
-
     /*
      * Controller class for patient interactions.
      *   register as a new patient.
--- src/main/java/application/ControllerPatientUpdate.java
+++ src/main/java/application/ControllerPatientUpdate.java
@@ -14,13 +14,6 @@ import view.PatientView;
 
 @Controller
 public class ControllerPatientUpdate {
-  /*TODO: Copy this class from Lab 19 and modify
-     Use ControllerDoctor.java for reference
-     Add Entity classes and Repository
-     Add Instance variables for SequenceService and repository interfaces
-
-     You may make changes to any of the model classes and repository interfaces as needed and add any customized query methods
-   */
 
     @Autowired
     private PatientRepository patientRepository;
